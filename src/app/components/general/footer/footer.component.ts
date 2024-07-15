29350ecb90de557fae2c99dbdbc3776d487ff680
Ankush <ankushrana98145@gmail.com>
2024-07-15 17:48:31 +0530

    add footer page

--- src/app/components/general/footer/footer.component.ts
+++ src/app/components/general/footer/footer.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, query, stagger, animate, style, transition } from '@angular/animations'
+import { GoogleAnalyticsService } from 'ngx-google-analytics';
+import { AnaylticsService } from '../../../services/anayltics.service';
+
+
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
+  animations:[
+    trigger("animateFooter", [
+      transition(":enter", [
+        query("*", [
+          style({opacity: 0, transform: "translateY(100%)"}),
+          stagger(50, [
+            animate(
+              "250ms cubic-bezier(0.35, 0, 0.25, 1)",
+              style({opacity:1, transform: "none"})
+            )
+          ])
+        ])
+      ])
+    ])
+  ]
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  constructor(
+    public analyticsService: AnaylticsService
+  ) { }
+
+  ngOnInit(): void {
+  }
 
 }
